faef478de151a0ae5cfcd9635dd0ebfbe1a9b0c8
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2022-08-08 16:16:38 +0000

    add hover effect on each div to leave trail via JS

--- index.js
+++ index.js
@@ -1,39 +1,60 @@
 let body = document.querySelector("body");
+
+let squareSideCount = 16;
+let groupCount = squareSideCount;
+let containerWidth = 80; //vw
+let containerHeight = 80; //vh
+let groupHeight = (containerHeight - 5) / groupCount; //vh
+let standardLength = groupHeight; //vh and vw
+
 //let containerDiv = document.querySelector(".container");
 //let grouperDiv = document.querySelector(".group");
 //let standardDiv = document.querySelector(".standard");
-//16 by 16 div is a total of 256 div
-//Using Flex, you will need 16 flex-direction-row groups inside a flex-direction-column container
 
+function createNewGrid () {
 
-
-
-
-function create256Grid () {
-
-//Step 1 - Create container with flex direction column
-    let container = document.createElement("div");
-    container.classList.add("container");
-    body.appendChild(container);
-
-//Step 2 - Create 16 sets of grouping-div with flex direction row
-    for (let i = 0; i < 16; i++) {
-        let grouper = document.createElement("div");
-        grouper.classList.add("group");
-        container.appendChild(grouper);
-
-//Step 3 - For each grouping-div, create 16 div within them
-        for (let j = 0; j < 16; j++) {
-            let standard = document.createElement("div");
-            standard.classList.add("standard");
-            grouper.appendChild(standard);
+    //Step 1 - Create container with flex direction column and set container width
+        let container = document.createElement("div");
+        container.classList.add("container");
+        container.style.width = containerWidth + "vw";
+        container.style.height = containerHeight + "vh";
+        body.appendChild(container);
+    
+    //Step 2 - Create groupCount sets of grouping-div with flex direction row and set grouper height
+        for (let i = 0; i < groupCount; i++) {
+            let grouper = document.createElement("div");
+            grouper.classList.add("group");
+            grouper.style.height = groupHeight +"vh";
+            container.appendChild(grouper);
+    
+    //Step 3 - For each grouping-div, create 16 standard div within them and set standardLength as width and height
+            for (let j = 0; j < groupCount; j++) {
+                let standard = document.createElement("div");
+                standard.classList.add("standard");
+                standard.style.width = standardLength + "vw";
+                standard.style.height = standardLength + "vh";
+                grouper.appendChild(standard);
+            }
         }
+
+    //Step 4 - Add event listener on each standard div for hover effect
+    let standardDiv = document.querySelectorAll(".standard");
+    standardDiv.forEach((item) => {
+        item.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "#252526";
+        })
+    });
+
     }
-}
 
-create256Grid();
+    //Call the function 
+    createNewGrid();
+    
+    
+   
+    
 
-
+    
 
 
  
